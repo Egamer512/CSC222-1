9a796a17793665638b67c19d052f9659c18d170e
ledmer <ledmer13@gmail.com>
2023-09-07 12:37:32 -0400

    Rearrange of functions

---
+++ Excercises/ch03/new_line.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+void newLine();
+
+int main()
+{
+    cout << "First Line." << endl;
+    newLine();
+    cout << "Second Line." << endl;
+    return 0;
+}
+
+void newLine()
+{
+    cout << endl;
+}
