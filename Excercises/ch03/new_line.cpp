8b3f0189ebc4e9459ae06c5542b8eff5eb4548a6
ledmer <ledmer13@gmail.com>
2023-09-07 12:50:35 -0400

    name of executable fixed

--- Excercises/ch03/new_line.cpp
+++ Excercises/ch03/new_line.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-void newLine();
+void new_line();
 
 int main()
 {
     cout << "First Line." << endl;
-    newLine();
+    new_line();
     cout << "Second Line." << endl;
     return 0;
 }
-
-void newLine()
+void new_line()
 {
     cout << endl;
 }
