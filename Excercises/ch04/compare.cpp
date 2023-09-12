8674d114ef8af2d8060472ecf30367884462161c
ledmer <ledmer13@gmail.com>
2023-09-11 22:15:09 -0400

    homework done, still need to clean up

--- Excercises/ch04/compare.cpp
+++ Excercises/ch04/compare.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 using namespace std;
 
 void compare(int a, int b){
@@ -14,14 +15,29 @@ void compare(int a, int b){
 		cout << a << " it's the same as " << b << endl;
 	}
 }
-int main (int argc, char **argv){
+bool check_if_number(char *a){
+	for (int i = 0, len_a = strlen(a); i < len_a; i++){
+		int part = a[i];
+		if ((part < 48 || part > 57) && (part != 45)){		
+			return true;
+		}
+	}	
+	return false;
+}
+int main (int argc, char *argv[]){
 //I thought that it will be boring if the computer asked you for prompts
 	if (argc != 3){
-		cout << "Usage: ./compare num1 num2" << endl;
-	return 1;
+		cout << "Usage: ./compare int1 int2" << endl;
+		return 1;
 	}
-	int a = atoi(argv[argc - 2]);
-	int b = atoi(argv[argc - 1]);
+
+	for (int i = 2; i > 0; i--){
+		if(check_if_number(argv[i])){
+			cout << "Usage:1./compare int1 int2" << endl;
+			return 1;
+		}		
+	}
+	int a = atoi(argv[1]), b = atoi(argv[2]);
 	compare(a, b);
 	return 0;
 }
