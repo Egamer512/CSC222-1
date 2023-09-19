430d17d97d3a8e37225bd6cc88e7fe53eeb18e2a
ledmer <ledmer13@gmail.com>
2023-09-19 11:14:24 -0400

    using find in looping_and_counting

--- Excercises/ch07/looping_and_counting.cpp
+++ Excercises/ch07/looping_and_counting.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
-int count_letters (string string,char letter){
+int count_letters (string str,char letter){
     int count = 0;
-    int index = 0;
-
-    while (index < string.length()) {
-        if (string[index] == letter) {
-            count ++;
-        }
-        index ++;
+    int index = str.find(letter);
+    int check;
+    if (letter >= 65 && letter <= 90){
+        check = 32;
     }
+    if (letter >= 97 && letter <= 122){
+        check = -32;
+    }
+    while (index != -1){
+        count ++;
+        index = str.find(letter, index + 1);
+    }
+    index = str.find(letter + check);
+    while (index != -1){
+        count ++;
+        index = str.find(letter + check, index + 1 );
+    }       
     return count;
 }
 int main(){
-    string str = "ASLDJASLFJLSiiiiiiA";
-    
-    char letter = 'i';
-    cout << count_letters(str, letter);
-
+    string str;
+    cout << "Give me a string: ";
+    cin >> str;
+    char letter;
+    cout << "Give me a letter to find: ";
+    cin >> letter;
+    while (!((letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122))){
+        cin >> letter;
+    }
+    cout << "the letter "<< letter << " appears " << count_letters(str, letter) << " times";
 }
