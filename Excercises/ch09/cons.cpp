aaccf9963934d93a618753770308ce41ecd3271b
ledmer <ledmer13@gmail.com>
2023-09-28 09:15:58 -0400

    cons

---
+++ Excercises/ch09/cons.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Car {
+    char *brand, *model, *engine;
+};
+
+int main (void){
+    Car car = {"Ferrari", "Ferrari 250 GTO", "Tipo 168/62 Comp. 3.0 L (2,953 cc) V12"}
+    print_car(car &)
+}
+
+int print_car (const Car& c){
+    c.brand = "Mercedes"
+    print()
+}
