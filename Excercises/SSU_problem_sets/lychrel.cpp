0f0893cbb1e33692b20561ec474ab47b413994d3
ledmer <ledmer13@gmail.com>
2023-09-28 09:11:42 -0400

    lych

--- Excercises/SSU_problem_sets/lychrel.cpp
+++ Excercises/SSU_problem_sets/lychrel.cpp
@@ -2,16 +2,17 @@
 #include <cmath>
 
 using namespace std;
-long reverse (long num){
-    long reversed_number = 0, reversed_num = 0;
-    int remainder = 0;
-    while (num != 0){
-        remainder = num % 10;
-        reversed_num = (reversed_num * 10) + remainder;
-        num /= 10;
-    }
-    return reversed_num;
+long reverse (long num);
+int lychrel (long num);
+
+int main(){
+    cout << "Let's Check if you can find a Lychrel number after 10 iterations\n" << "Enter a number:";
+    long num;
+    cin >> num;
+    lychrel(num);
+    return 0;
 }
+
 int lychrel (long num){
     
     for (int i = 1; i <= 10; i++){
@@ -29,10 +30,13 @@ int lychrel (long num){
     cout << "Still a Lychrel number after "<< 10 << " iterations: "<< num << endl;
     return 1;
 }
-int main(){
-    cout << "Let's Check if you can find a Lychrel number after 10 iterations\n" << "Enter a number:";
-    long num;
-    cin >> num;
-    lychrel(num);
-    return 0;
+
+long reverse (long num){
+    long reversed_num = 0, remainder;
+    while (num != 0){
+        remainder = num % 10;
+        reversed_num = (reversed_num * 10) + remainder;
+        num /= 10;
+    }
+    return reversed_num;
 }
