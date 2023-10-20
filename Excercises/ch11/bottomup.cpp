0d1fdbd9260bb4a34ccf90ce3df6a66f62376635
ledmer <ledmer13@gmail.com>
2023-10-19 21:47:01 -0400

    final bottomup

---
+++ Excercises/ch11/bottomup.cpp
@@ -0,0 +1,80 @@
+// Copies a BMP file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // Ensure proper usage
+    if (argc != 3)
+    {
+        printf("Usage: copy infile outfile\n");
+        return 1;
+    }
+
+    // Remember filenames
+    char *infile = argv[1];
+    char *outfile = argv[2];
+    
+    // Open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // Open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        printf("Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // Read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // Read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    bi.biHeight = -bi.biHeight;
+    // Write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // Write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // Iterate over infile's scanlines
+    int biHeight = abs(bi.biHeight), biWidth = bi.biWidth;
+
+    for (int i = 0; i < biHeight; i++)
+    {
+        // Iterate over pixels in scanline
+        for (int j = 0; j < biWidth; j++)
+        {
+            // Temporary storage
+            RGBTRIPLE triple;
+
+            // Read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+            // Write RGB triple to outfil
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+        }
+    }
+
+    // Close infile
+    fclose(inptr);
+
+    // Close outfile
+    fclose(outptr);
+
+    // Success
+    return 0;
+}
