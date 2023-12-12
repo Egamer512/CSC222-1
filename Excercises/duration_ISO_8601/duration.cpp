a7bdc112e00acd80d3c98de7ac2a9a113f3507c8
ledmer <ledmer13@gmail.com>
2023-12-12 18:41:19 -0500

    not finished yet

--- Excercises/duration_ISO_8601/duration.cpp
+++ Excercises/duration_ISO_8601/duration.cpp
@@ -1,1 +1,16 @@
-#include 'Duration.h'
+#include "Duration.h"
+
+string Duration::to_string ()const{
+    string T = "PT";
+    if (seconds >= 3600){
+        T.append(std::to_string(int(seconds/3600)) + "H");
+        seconds = int(seconds) % 3600;
+    }
+    if (seconds >= 60){
+        T.append(std::to_string(int(seconds/60))+ "M");
+        seconds = int(seconds) % 60;
+    }
+    T.append(std::to_string(int(seconds)) + "S");
+    return T;
+}
+Duration Duration::to_string ()const{
