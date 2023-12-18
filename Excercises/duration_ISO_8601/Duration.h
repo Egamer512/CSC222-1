4c7a3b2b87c4ddf18715fc3b48a07af3520e78eb
ledmer <ledmer13@gmail.com>
2023-12-17 22:31:53 -0500

    duration done with new test

--- Excercises/duration_ISO_8601/Duration.h
+++ Excercises/duration_ISO_8601/Duration.h
@@ -3,12 +3,21 @@
 #include <string>
 using namespace std;
 class Duration{
-    mutable double seconds;
+    mutable double sec;
+    int hour(void);
+    int minute(void);
+    int seco(void);
     public:
-    Duration() : seconds(0){}
-    Duration(double s) : seconds(s) {}
+    Duration() : sec(0){}
+    Duration(double s) : sec(s) {}
+
+    int days(void);
+    int hours(void);
+    int minutes(void);
+    int seconds(void);
+
 
     string to_string () const;
-    Duration operator + (Duration);
-
+    Duration operator + (Duration T);
+    Duration operator - (Duration T);
 };
--- Excercises/duration_ISO_8601/duration.cpp
+++ Excercises/duration_ISO_8601/duration.cpp
@@ -1,19 +1,72 @@
 #include "Duration.h"
 
 string Duration::to_string ()const{
-    string T = "PT";
-    if (seconds >= 3600){
-        T.append(std::to_string(int(seconds/3600)) + "H");
-        seconds = int(seconds) % 3600;
+    string T = "P";
+    if (int(sec) <= 0){
+        T.append("T0S");
+        return T;
     }
-    if (seconds >= 60){
-        T.append(std::to_string(int(seconds/60))+ "M");
-        seconds = int(seconds) % 60;
+    if (sec >=  86400){
+        T.append(std::to_string(int(sec/86400)) + "D");
+        sec = int(sec) % 86400;
     }
-    T.append(std::to_string(int(seconds)) + "S");
+    T.append("T");
+    if (sec >= 3600){
+        T.append(std::to_string(int(sec/3600)) + "H");
+        sec = int(sec) % 3600;
+    }
+    if (sec >= 60){
+        T.append(std::to_string(int(sec/60))+ "M");
+        sec = int(sec) % 60;
+    }
+    if (sec > 0){
+        T.append(std::to_string(int(sec)) + "S");
+    }
     return T;
 }
+int  Duration::days(void){
+    if (sec >=  86400){
+        return(int(sec) / 86400);
+    }
+    return 0;
+}
+
+int  Duration::hours(void){
+    int h = hour();
+    if (h >= 3600){
+        return(h/3600);
+    }
+    return 0;
+}
+int  Duration::minutes(void){
+    int m = minute();
+    if (m >= 60){
+        return(m/60);
+    }
+    return 0;
+}
+int  Duration::seconds(void){
+    int s = seco();
+    if (s > 0){
+        return(s);
+    }
+    return 0;
+}
+int Duration::hour(void){
+    return (int(sec) % 86400);
+}
+int Duration::minute(void){
+    return((hour()) % 3600);
+}
+int Duration::seco(void){
+    return((minute()) % 60);
+}
+
 Duration Duration::operator + (Duration T){
-    Duration Time(seconds + T.seconds);
+    Duration Time(sec + T.sec);
+    return Time;
+}
+Duration Duration::operator - (Duration T){
+    Duration Time(sec - T.sec);
     return Time;
 }
